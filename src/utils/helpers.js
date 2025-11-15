556a1bed76a2d0b550808221f214cd557682cc24
Hamza Hajjaji <hh.hajjaji.hamza@gmail.com>
2025-11-15 21:19:21 +0100

    feat(utils): ajouter un paramètre de préfixe à generateUUID
    - Fonction generateUUID accepte maintenant un paramètre de préfixe optionnel
    - Le préfixe est concaténé à l'UUID généré
    - Ceci permet d'avoir des UUID avec un préfixe personnalisé lors de leur génération

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,9 +2,10 @@ import { v4 as uuidv4 } from '../../node_modules/uuid';
 
 /**
  * Génère une chaîne UUID
+ * @param {string} [prefix] - Le préfixe à ajouter à l'UUID
  * @returns {string} Retourne une chaîne UUIDv4
  */
-export function generateUUID() {
+export function generateUUID(prefix = '') {
     // Appelle la fonction uuidv4 pour générer l'UUID
-    return uuidv4();
+    return `${prefix}${uuidv4()}`;
 }
