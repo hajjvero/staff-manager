35eddbbbe9385ac947b62bc20cf2097ada3db562
Hamza Hajjaji <hh.hajjaji.hamza@gmail.com>
2025-11-15 21:29:50 +0100

    feat(utils): add constants for user roles
    - Create constants file for user roles in a restaurant context
    - Define and export frozen object with role names in French

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const ROLES = Object.freeze({
+    RECEPTION: 'Réception',
+    SERVEUR: 'Serveurs',
+    SECURITE: 'Sécurité',
+    MANAGER: 'Manager',
+    NETTOYAGE: 'Nettoyage',
+    AUTRES: 'Autres'
+})
