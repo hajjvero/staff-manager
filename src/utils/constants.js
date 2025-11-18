a3938f9e0829868b5d4a24ffd86483390136661c
Hamza Hajjaji <hh.hajjaji.hamza@gmail.com>
2025-11-18 13:21:45 +0100

    refactor: remove server role and adjust staff list layout
    - Remove SERVEUR role from constants
    - Shorten minimum height of unassigned staff list
    - Remove SERVEUR option from worker role selection

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,6 @@
 export const ROLES = Object.freeze({
     RECEPTIONISTE: 'Réceptionniste',
     IT: 'Techniciens IT',
-    SERVEUR: 'Serveurs',
     SECURITE: 'Agent de sécurité',
     MANAGER: 'Manager',
     NETTOYAGE: 'Nettoyage',
